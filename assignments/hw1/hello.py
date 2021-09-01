9340de2d0b633cdf917c539e1794ea3a9ea663d0
maisiehester <hesterm@g.cofc.edu>
2021-09-01 17:23:01 -0500

    This is the completed HW1!

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,13 @@
+"""
+Name: Maisie Hester
+hello.py
+
+Problem: this illustrates how to print hello world!
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+def main():
+    print("hello, world!")
+
+main()
